8cac0967dc6a685d27a84635b88d55735584e7b0
Roger Waldron <roger.waldron@gmail.com>
2023-02-24 01:14:35 +0100

    Initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'ng15-movies-app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'ng15-movies-app';
+}
